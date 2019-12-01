0311b359ec422c2303e1a105ce74dc0c36f29c05
Jakob <42341254+JLMadsen@users.noreply.github.com>
2019-12-01 13:21:15 +0100

    removed result

--- day1.py
+++ day1.py
@@ -1,11 +1,10 @@
 """
 https://adventofcode.com/2019/day/1
 
-part 1: 3465245
-part 2: 5194970
 """
 
-def main() -> None:
+# takes part as argument, int 1 or 2, depending on which task to run.
+def main(part) -> None:
     filename = "day1input.txt"
     planets = open(filename).read().split()
     
@@ -14,14 +13,18 @@ def main() -> None:
     for planet in planets:
         fuel = [int(int(planet)/3)-2]
 
-        while True:
-            new_fuel = int(fuel[-1]/3)-2
-            if(new_fuel <= 0): break
-            fuel.append(new_fuel)
-            
-        for f in fuel: sum += f
+        if part == 1:
+            sum += fuel[0]
+        else:
+            while True:
+                new_fuel = int(fuel[-1]/3)-2
+                if(new_fuel <= 0): break
+                fuel.append(new_fuel)
+                
+            for f in fuel: sum += f
         
     print(str(sum))
 
 if __name__ == '__main__':
-    main()
+    part = 1
+    main(part)
