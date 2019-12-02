a6dc1f2a70de4301352f65210f37602156157d46
Jakob <42341254+JLMadsen@users.noreply.github.com>
2019-12-02 10:03:46 +0100

    Update day2.py

--- day2.py
+++ day2.py
@@ -38,8 +38,10 @@ def part2() -> []:
 
     ints = readIntcodesFromFile()
     
+    # determine what pair of inputs produces the output 19690720
     for a in range(100):
         for b in range(100):
+            
             cpy = ints.copy()
             cpy[1] = a
             cpy[2] = b
@@ -47,6 +49,9 @@ def part2() -> []:
             if IntcodeCompiler(cpy) == 19690720:
                 return [a, b]
 
-if __name__ == '__main__':
+def main() -> None:
     print('Task 1:', part1())
     print('Task 2:', part2())
+    
+if __name__ == '__main__':
+    main()
