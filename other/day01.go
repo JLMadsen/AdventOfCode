57351a703a4a7dd7481da1017d109d949baf4630
Jakob <42341254+JLMadsen@users.noreply.github.com>
2019-12-23 17:56:00 +0100

    Update day01.go

--- other/day01.go
+++ other/day01.go
@@ -3,17 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
-
-func part1(dat string[]) (totalFuel int) {
-
-	for _, element := range dat {
-		number := int(element)
-		totalFuel += int(number/3) - 2
-	}
-	return
-}
 
 func check(e error) {
 	if e != nil {
@@ -21,13 +13,54 @@ func check(e error) {
 	}
 }
 
+func fuelPerMass(mass int) (fuel int) {
+	fuel = int(mass/3) - 2
+	return
+}
+
+func part1(dat []string) (totalFuel int) {
+
+	for _, element := range dat {
+		mass, err := strconv.Atoi(element)
+		check(err)
+		totalFuel += fuelPerMass(mass)
+	}
+	return
+}
+
+func part2(dat []string) (totalFuel int) {
+
+	for _, element := range dat {
+		mass, err := strconv.Atoi(element)
+		check(err)
+
+		for true {
+
+			fuel := fuelPerMass(mass)
+
+			if fuel > 0 {
+				totalFuel += fuel
+				mass = fuel
+			} else {
+				break
+			}
+		}
+	}
+	return
+}
+
 func main() {
 	filename := "./input/day1input.txt"
 	dat, err := ioutil.ReadFile(filename)
 	check((err))
 
-	masses := strings.Split(string(dat), "\n")
+	// file uses CRLF so split on \r\n
+	// change to \r if carriage return or \n if line feed
+	masses := strings.Split(string(dat), "\r\n")
 
 	result := part1(masses)
 	fmt.Println("Part 1 = ", result)
+
+	result = part2(masses)
+	fmt.Println("Part 2 = ", result)
 }
