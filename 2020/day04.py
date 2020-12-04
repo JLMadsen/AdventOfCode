66b8716db002e19597982cdbdfff4252abdd784d
Jakob <jakob.m@dsen.tv>
2020-12-04 09:26:37 +0100

    Update day04.py

--- 2020/day04.py
+++ 2020/day04.py
@@ -1,9 +1,9 @@
 import re
 
-data = { 'byr': lambda x: x <= '2002' and x >= '1920',
-         'iyr': lambda x: x <= '2020' and x >= '2010',
-         'eyr': lambda x: x <= '2030' and x >= '2020',
-         'hgt': lambda x: (x:=x[:-2]) <= '76' and x >= '59' if 'in' in x else (x:=x[:-2]) <= '193' and x >= '150' if x[-2:] in ['cm','in'] else False,
+data = { 'byr': lambda x: '1920' <= x <= '2002',
+         'iyr': lambda x: '2010' <= x <= '2020',
+         'eyr': lambda x: '2020' <= x <= '2030',
+         'hgt': lambda x: '59' <= x[:-2] <= '76' if 'in' in x else '150' <= x[:-2] <= '193' if x[-2:] in ['cm','in'] else False,
          'hcl': lambda x: re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', x),
          'ecl': lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
          'pid': lambda x: len(x)==9 and not re.search(r'\D', x) }
