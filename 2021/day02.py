f2c2f3e6f7d94ca7a399143e1e59e3f1f80ca374
Jakob <jakob.m@dsen.tv>
2021-12-02 17:21:43 +0100

    Update day02.py

--- 2021/day02.py
+++ 2021/day02.py
@@ -3,21 +3,11 @@ import math
 def get_pos(inst, x=0, y=0):
     return math.prod([(x:=x+((int(b[1]) * (1 if b[0] in 'down' else -1)) if b[0] in ['down', 'up'] else 0), y:=y+(int(b[1]) if b[0] in ['forward'] else 0)) for line in inst if (b:=line.split())][-1])
 
-def get_pos2(inst):
-    aim = hor = depth = 0
-    for line in inst:
-        if('down' in line):
-            aim += int(line.split()[-1])
-        elif('up' in line):
-            aim -= int(line.split()[-1])
-        elif('forward' in line):
-            hor += int(line.split()[-1])
-            depth += aim * int(line.split()[-1])
-    return depth * hor
-
+def get_pos2(inst, x=0, y=0, d=0):
+    return math.prod([(d:=d+((int(b[1]) * (1 if b[0] in 'down' else -1)) if b[0] in ['down', 'up'] else 0), y:=y+(int(b[1]) if b[0] in ['forward'] else 0), x:=x+(d*int(b[1]) if b[0] in ['forward'] else 0)) for line in inst if (b:=line.split())][-1][1:])
 
 if __name__ == "__main__":
-    with open('2021/input/day02.txt') as f:
+    with open('input/day02.txt') as f:
         content = f.read().split('\n')[:-1]
 
         print(get_pos(content)) # 1451208
