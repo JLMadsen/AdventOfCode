db91ccfded80bc6af0d52b2a88bfa8dac8148b76
Jakob <jakob.m@dsen.tv>
2021-12-02 16:55:16 +0100

    Update day02.py

--- 2021/day02.py
+++ 2021/day02.py
@@ -1,15 +1,7 @@
+import math
 
-def get_pos(inst):
-    ver = hor = 0
-    for line in inst:
-        if('down' in line):
-            ver += int(line.split()[-1])
-        elif('up' in line):
-            ver -= int(line.split()[-1])
-        elif('forward' in line):
-            hor += int(line.split()[-1])
-    return ver * hor
-
+def get_pos(inst, x=0, y=0):
+    return math.prod([(x:=x+((int(b[1]) * (1 if b[0] in 'down' else -1)) if b[0] in ['down', 'up'] else 0), y:=y+(int(b[1]) if b[0] in ['forward'] else 0)) for line in inst if (b:=line.split())][-1])
 
 def get_pos2(inst):
     aim = hor = depth = 0
@@ -25,8 +17,8 @@ def get_pos2(inst):
 
 
 if __name__ == "__main__":
-    with open('input/day02.txt') as f:
+    with open('2021/input/day02.txt') as f:
         content = f.read().split('\n')[:-1]
 
-        print(get_pos(content))
-        print(get_pos2(content))
+        print(get_pos(content)) # 1451208
+        print(get_pos2(content)) # 1620141160
