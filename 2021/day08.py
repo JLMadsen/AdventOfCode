6659067076496598676cec364c48c0d0b3f790cd
Jakob Madsen <>
2021-12-08 11:54:26 +0100

    Update day08.py

--- 2021/day08.py
+++ 2021/day08.py
@@ -1,12 +1,5 @@
 def part1(numbers):
-    count = 0
-    digits = [2, 3, 4, 7]
-    for line in numbers:
-        signal, output = line.split('|')
-        for value in output.split():
-            if len(value) in digits:
-                count += 1
-    return count
+    return sum([sum([1 if len(string) in [2,3,4,7] else 0 for string in line.split('|')[1].split(' ')]) for line in numbers])
 
 def count_common(s1, s2):
     return len(list(set(s1).intersection(s2)))
@@ -34,48 +27,46 @@ def part2(numbers):
                 continue
 
             string = "".join(sorted(s))
-            common_digits = [ count_common(string, digits[i]) if i in [1,4,7,8] else 0 for i in range(10) ]
+            intersect = [ count_common(string, digits[i]) if i in [1,4,7,8] else 0 for i in range(10) ]
 
-            if (common_digits[1] == 2 and
-                common_digits[4] == 3 and
-                common_digits[7] == 3 and
-                common_digits[8] == 6 and
+            if (intersect[1] == 2 and
+                intersect[4] == 3 and
+                intersect[7] == 3 and
+                intersect[8] == 6 and
                 len(string) == 6):
                 digits[0] = string
 
-            elif (common_digits[1] == 1 and
-                  common_digits[4] == 2 and
-                  common_digits[7] == 2 and
-                  common_digits[8] == 5 and
+            elif (intersect[1] == 1 and
+                  intersect[4] == 2 and
+                  intersect[7] == 2 and
+                  intersect[8] == 5 and
                   len(string) == 5):
                 digits[2] = string
 
-            elif common_digits[1] == 2 and len(string) == 5:
+            elif intersect[1] == 2 and len(string) == 5:
                 digits[3] = string
 
-            elif (common_digits[1] == 1 and
-                 common_digits[4] == 3 and
-                 common_digits[7] == 2 and
-                 common_digits[8] == 5 and
-                 len(string) == 5):
+            elif (intersect[1] == 1 and
+                  intersect[4] == 3 and
+                  intersect[7] == 2 and
+                  intersect[8] == 5 and
+                  len(string) == 5):
                 digits[5] = string
 
-            elif (common_digits[1] == 1 and
-                  common_digits[4] == 3 and
-                  common_digits[7] == 2 and
-                  common_digits[8] == 6 and
+            elif (intersect[1] == 1 and
+                  intersect[4] == 3 and
+                  intersect[7] == 2 and
+                  intersect[8] == 6 and
                   len(string) == 6):
                 digits[6] = string
 
-            elif common_digits[4] == 4 and len(string) == 6:
+            elif intersect[4] == 4 and len(string) == 6:
                 digits[9] = string
 
         buffer = ""
         for o in output.split(' '):
             string = "".join(sorted(o))
-            if string not in digits:
-                continue
-            buffer += str( digits.index(string) )
+            buffer += str( digits.index(string) if string in digits else "" )
         total += int(buffer)
 
     return total
