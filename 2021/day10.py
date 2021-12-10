9cb370d3a77308b1d5e42deec0d08736c5984f6a
Jakob Madsen <>
2021-12-10 09:00:44 +0100

    Update day10.py

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,37 +1,30 @@
-opening_brackets = ['(', '[', '{', '<']
-closing_brackets = [')', ']', '}', '>']
+opening          = ['(', '[', '{', '<']
+closing          = [')', ']', '}', '>']
 score            = [3, 57, 1197, 25137]
 incomplete_lines = []
 
 def get_stack(line):
-    global incomplete_lines
     stack = []
     for char in line:
-        if char in opening_brackets:
+        if char in opening:
             stack.append(char)
         else:
-            if char != closing_brackets[ opening_brackets.index( stack.pop() ) ]:
+            if char != closing[ opening.index( stack.pop() ) ]:
                 return char
     incomplete_lines.append(stack)
 
 def parse(lines):
     error = [ char for line in lines if (char:=get_stack(line)) ]    
-    points = 0
-
-    for idx, char in enumerate(closing_brackets):
-        points += score[idx] * error.count(char)
-
-    return points
+    return sum([ score[idx] * error.count(char) for idx, char in enumerate(closing)])
 
 def finish_lines():
-    global incomplete_lines
     points = []
     for stack in incomplete_lines:
         points.append(0)
         for char in reversed(stack):
-            points[-1] = points[-1] * 5 + opening_brackets.index(char) + 1
+            points[-1] = points[-1] * 5 + opening.index(char) + 1
 
-    return sorted(points)[ (len(points) -1 )// 2 ]
+    return sorted(points)[ (len(points) -1 ) // 2 ]
 
 if __name__ == "__main__":
     with open('input/day10.txt') as f:
