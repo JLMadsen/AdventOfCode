d817aabde2166bbe4df423d393af97afc4160e46
Jakob Madsen <>
2021-12-14 11:57:16 +0100

    Update day14.py

--- 2021/day14.py
+++ 2021/day14.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 def process(template, insertions, steps):
     counter = Counter([a + b for a, b in zip(template, template[1:])])
 
-    for step in range(steps):
+    for _ in range(steps):
         new_counter = Counter()
 
         for pair in counter:
@@ -19,8 +19,10 @@ def process(template, insertions, steps):
     chars = defaultdict(lambda: 0)
     for pair in counter:
         chars[pair[0]] += counter[pair]
+
+    chars[ template[-1] ] += 1
     
-    print( max(chars.values()) - min(chars.values()) + 1 )
+    print( max(chars.values()) - min(chars.values()) )
 
 if __name__ == "__main__":
     with open('input/day14.txt') as f:
