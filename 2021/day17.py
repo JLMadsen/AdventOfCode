c38f11a89cb62ef5ee2e29297916ffbe2b1154d3
Jakob Madsen <>
2021-12-17 13:19:21 +0100

    Update day17.py

--- 2021/day17.py
+++ 2021/day17.py
@@ -1,10 +1,11 @@
-def simulate(target_area):
-    x_target, y_target = target_area
+import re
+
+def simulate(left, right, down, up):
     vecs = set()
     y_max = 0
 
     for dy in range(-200, 200):
-        for dx in range(x_target[1] + 1):
+        for dx in range(right + 1):
             vy, vx = dy, dx
             local_y_max = y = x = 0
 
@@ -15,13 +16,13 @@ def simulate(target_area):
                 if local_y_max < y:
                     local_y_max = y
 
-                if ( x_target[0] <= x <= x_target[1] and 
-                     y_target[0] <= y <= y_target[1] ):
+                if ( left <= x <= right and 
+                     down <= y <= up ):
 
                     vecs.add((dy, dx))
                     y_max = local_y_max if y_max < local_y_max else y_max
 
-                if x_target[1] < x or y < y_target[0]:
+                if right < x or y < down:
                     break
 
                 vy -= 1
@@ -32,8 +33,5 @@ def simulate(target_area):
 
 if __name__ == "__main__":
     with open('input/day17.txt') as f:
-        content = f.read().strip()[15:].split(',')
-        content[1] = content[1].split('=')[1]
-        content = [*map(lambda n: [*map(int, n.split('..'))], content)]
-
-        simulate(content)
+        bounds = map(int, re.findall(r'-?\d+', f.read()))
+        simulate(*bounds)
