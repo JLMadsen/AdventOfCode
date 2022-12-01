de3a1a6226f5a61569096c64cb2d825c9e30f3d2
Jakob Madsen <>
2022-12-01 15:06:13 +0100

    Update day01.py

--- 2022/day01.py
+++ 2022/day01.py
@@ -1,5 +1,4 @@
-def solve(content):
-    elves = [0]
+def solve(content, elves = [0]):
     [elves.__setitem__(-1, elves[-1] + int(cal)) if cal else elves.append(0) for cal in content]
     print(max(elves)) # 68775
     print(sum(sorted(elves, reverse=True)[:3])) # 202585
