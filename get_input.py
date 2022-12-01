db16591bfeec4b4bbd7b453a65ac88e8183c27e5
Jakob <jakob.m@dsen.tv>
2022-12-01 17:04:10 +0100

    Update get_input.py

--- get_input.py
+++ get_input.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
 import os
-import requests 
+import requests
 import sys
 
 # get session cookie from website
-TOKEN = os.environ['AOC_TOKEN']
+TOKEN = os.environ["AOC_TOKEN"]
 CREATE_SCRIPT_FILE = True
 SCRIPT_CONTENT = """
 import math
@@ -18,30 +18,31 @@ def part1(content):
 
 if __name__ == "__main__":
     with open('input/day{}.txt') as f:
-        content = f.read().split('\\n')[:-1]
+        content = f.read().splitlines()
         part1(content)
 """
 
 def get_input(year, day):
     url = f"https://adventofcode.com/{year}/day/{day}/input"
-    data = requests.get(url, cookies={'session': TOKEN}).text
+    data = requests.get(url, cookies={"session": TOKEN}).text
 
     if "Please log in to get your puzzle input" in data:
         print("Invalid login token")
         get_input(year, day)
 
     print(data)
-    
-    day = day if day >= 10 else '0'+str(day)
-    filename = f'{year}/input/day{day}.txt'
+
+    day = day if day >= 10 else "0" + str(day)
+    filename = f"{year}/input/day{day}.txt"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         f.write(data)
 
     if CREATE_SCRIPT_FILE:
-        filename = f'{year}/day{day}.py'
-        with open(filename, 'w') as f:
+        filename = f"{year}/day{day}.py"
+        with open(filename, "w") as f:
             f.write(SCRIPT_CONTENT.format(day))
+
 
 # run as main to download todays input
 if __name__ == "__main__":
