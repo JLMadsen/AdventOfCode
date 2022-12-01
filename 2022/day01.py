96e4d4d480e941a526cd631c5276ff7dc315635b
Jakob <jakob.m@dsen.tv>
2022-12-01 08:35:49 +0100

    grammar

--- 2022/day01.py
+++ 2022/day01.py
@@ -1,11 +1,11 @@
 def solve(content):
-    elfs = [0]
+    elves = [0]
     for cal in content:
-        if not cal: elfs.append(0)
-        else: elfs[-1] += int(cal)
+        if not cal: elves.append(0)
+        else: elves[-1] += int(cal)
 
-    print(max(elfs)) # 68775
-    print(sum(sorted(elfs, reverse=True)[:3])) # 202585
+    print(max(elves)) # 68775
+    print(sum(sorted(elves, reverse=True)[:3])) # 202585
 
 if __name__ == "__main__":
     with open('input/day01.txt') as f:
