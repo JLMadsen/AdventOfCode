4e640b354bf99eba40bb078971883e8bdd23cd2c
Jakob <jakob.m@dsen.tv>
2022-12-02 13:06:19 +0100

    Update day02.py

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,23 +1,21 @@
 moves = ['A', 'B', 'C']
-idx = lambda x: ord(x) - ord('A')
+idx     = lambda x: ord(x) - ord('A')
 decrypt = lambda x: chr(ord(x) - ord('X') + ord('A'))
-beats = lambda a, b: b == moves[ idx(a) - 1 ]
+beats   = lambda a, b: b == moves[ idx(a) - 1 ]
 
 def play(a, b):
     if   beats(a, b): return 0
     elif beats(b, a): return 6
     return 3
 
-def part1(content):
-    score = 0
+def part1(content, score = 0):
     for hands in content:
         a, b = hands.split()
         b = decrypt(b)
         score += play(a, b) + idx(b) + 1
     print(score)
 
-def part2(content):
-    score = 0
+def part2(content, score = 0):
     for hands in content:
         a, tactic = hands.split()
         b = ( moves[idx(a) - 2] if tactic == 'Z' else
