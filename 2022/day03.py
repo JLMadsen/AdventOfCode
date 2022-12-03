dd770ebf803cb02024d2b6d913848e6bb84ea988
Jakob <jakob.m@dsen.tv>
2022-12-03 13:40:07 +0100

    Update day03.py

--- 2022/day03.py
+++ 2022/day03.py
@@ -1,12 +1,12 @@
 value     = lambda char: ord(char.lower()) - ord('a') + 1 + (26 if char.isupper() else 0)
-split     = lambda _, x: (x[:len(x)//2], x[len(x)//2:])
+split     = lambda x: (x[:len(x)//2], x[len(x)//2:])
 intersect = lambda xs: set.intersection(*map(set, xs)).pop()
 
 def solve(content, func, loop):
-    print(sum([value(intersect(func(content, line))) for line in loop]))
+    print(sum([value(intersect(func(line))) for line in loop]))
 
 if __name__ == "__main__":
     with open('input/day03.txt') as f:
         content = f.read().splitlines()
         solve(content, split, content) # 8139
-        solve(content, lambda x, i:x[i:i+3], range(0, len(content), 3)) # 2668
+        solve(content, lambda x: content[x:x+3], range(0, len(content), 3)) # 2668
