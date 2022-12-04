380d2646c697cd273cc38c0ff5f12da6091ced9e
Jakob <jakob.m@dsen.tv>
2022-12-04 08:13:15 +0100

    Update day04.py

--- 2022/day04.py
+++ 2022/day04.py
@@ -1,10 +1,10 @@
 def solve(content, includes = 0, value = 0):
     for line in content:
         ax, ay, bx, by = map(int, line.replace('-', ',').split(','))
-        value += (1 if (ax>=bx and ay<=by) or 
-                       (bx>=ax and by<=ay) or 
-                       (bx>=ay and by<=ax and includes) or
-                       (by>=ax and bx<=ay and includes) else 0)
+        value += ((ax>=bx and ay<=by) or 
+                  (bx>=ax and by<=ay) or 
+                  (bx>=ay and by<=ax and includes) or
+                  (by>=ax and bx<=ay and includes)) * 1
     print(value)
 
 if __name__ == "__main__":
