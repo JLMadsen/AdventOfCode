690edcb869d5fc68f4bd8eea2c5345bb8a241753
Jakob Madsen <>
2022-12-05 12:44:59 +0100

    Update day05.py

--- 2022/day05.py
+++ 2022/day05.py
@@ -3,12 +3,11 @@ def solve(content, pt2 = 0, boxes = []):
         if '[' in line:
             if not boxes: boxes = [[] for _ in range(len(line)//4+1)]
             for idx, char in enumerate(line[1::4]):
-                if char==' ' or char.isdigit(): continue
-                boxes[idx].insert(0, char)
+                boxes[idx].insert(0, char) if char != ' ' else None
         elif 'm' in line:
             n, source, dest = map(int, line.split(' ')[1::2])
             n = min(len(boxes[(source := source-1)]), n)
-            boxes[dest - 1] += boxes[source][-n:][::(1 if pt2 else -1)]
+            boxes[dest-1] += boxes[source][-n:][::(1 if pt2 else -1)]
             del boxes[source][-n:]
 
     print(''.join([stack[-1] for stack in boxes]))
