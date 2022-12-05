fc407f818d4578fbf9d15bd80e7aa23c114d324b
Jakob Madsen <>
2022-12-05 12:52:40 +0100

    Update day05.py

--- 2022/day05.py
+++ 2022/day05.py
@@ -1,4 +1,4 @@
-def solve(content, pt2 = 0, boxes = []):
+def solve(content, pt2 = False, boxes = []):
     for line in content:
         if '[' in line:
             if not boxes: boxes = [[] for _ in range(len(line)//4+1)]
@@ -7,7 +7,7 @@ def solve(content, pt2 = 0, boxes = []):
         elif 'm' in line:
             n, source, dest = map(int, line.split(' ')[1::2])
             n = min(len(boxes[(source := source-1)]), n)
-            boxes[dest-1] += boxes[source][-n:][::(1 if pt2 else -1)]
+            boxes[dest-1] += boxes[source][-n:][::(1-2*pt2)]
             del boxes[source][-n:]
 
     print(''.join([stack[-1] for stack in boxes]))
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     with open('input/day05.txt') as f:
         content = f.read().splitlines()
         solve(content) # NTWZZWHFV
-        solve(content, 1) # BRZGFVBTJ
+        solve(content, True) # BRZGFVBTJ
