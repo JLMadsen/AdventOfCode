10fabac95e746c5cdcdb39687c7e98d88fe2c8a5
Jakob <jakob.m@dsen.tv>
2022-12-05 08:37:13 +0100

    Update day05.py

--- 2022/day05.py
+++ 2022/day05.py
@@ -1,9 +1,8 @@
 def solve(content, crateMover = 0, boxes = [], setup = True):
-    
     for line in content:
         if not line:
             setup = False
-            boxes = [[*reversed(stack)] for stack in boxes]
+            boxes = [*map(lambda x: x[::-1], boxes)]
             continue
 
         if setup:
@@ -15,19 +14,9 @@ def solve(content, crateMover = 0, boxes = [], setup = True):
                 boxes[i // 4].append(box.strip()[1:-1])
         else:
             _, n, _, source, _, dest = line.split(' ')
-            source = int(source) - 1
-            dest = int(dest) - 1
-
-            pickup = []
-            for i in range(int(n)):
-                if len(boxes[source]) == 0:
-                    break
-
-                (pickup if crateMover else boxes[dest]).append(boxes[source][-1])
-                boxes[source].pop()
-
-            if crateMover:
-                boxes[dest] += [*reversed(pickup)]
+            n = min(len(boxes[(source := int(source)-1)]), int(n))
+            boxes[int(dest) - 1] += boxes[source][-n:][::(1 if crateMover else -1)]
+            [boxes[source].pop() for _ in range(n)]
 
     print(''.join([stack[-1] for stack in boxes]))
 
