3e84a8f36d9e41b6b314f2527703f1cd9b55c278
Jakob <jakob.m@dsen.tv>
2022-12-05 06:43:01 +0100

    Update day05.py

--- 2022/day05.py
+++ 2022/day05.py
@@ -1,47 +1,35 @@
-def solve(content, createMover9000 = 0):
-    boxes = []
-    setup = True
-
+def solve(content, crateMover = 0, boxes = [], setup = True):
+    
     for line in content:
-        if line == '':
+        if not line:
             setup = False
             boxes = [[*reversed(stack)] for stack in boxes]
             continue
 
         if setup:
-            layer = []
-            if len(boxes) == 0:
+            if not boxes:
                 boxes = [[] for _ in range(len(line)//4+1)]
             for i in range(0, len(line), 4):
                 box = line[i:i+4]
-                if '[' not in box:
-                    continue
-                idx = i // 4 + 1
-                boxes[idx-1].append(box.strip()[1:-1])
+                if '[' not in box: continue
+                boxes[i // 4].append(box.strip()[1:-1])
         else:
-            _, n, _, source, _, destination = line.split(' ')
-            n = int(n)
-            source = int(source)-1
-            destination = int(destination)-1
+            _, n, _, source, _, dest = line.split(' ')
+            source = int(source) - 1
+            dest = int(dest) - 1
 
             pickup = []
-            for i in range(n):
+            for i in range(int(n)):
                 if len(boxes[source]) == 0:
                     break
 
-                if createMover9000:
-                    pickup.append(boxes[source][-1])
-                else:
-                    boxes[destination].append(boxes[source][-1])
+                (pickup if crateMover else boxes[dest]).append(boxes[source][-1])
                 boxes[source].pop()
 
-            if createMover9000:
-                boxes[destination] += [*reversed(pickup)]
+            if crateMover:
+                boxes[dest] += [*reversed(pickup)]
 
-    result = ''
-    for stack in boxes:
-        result += stack[-1]
-    print(result)
+    print(''.join([stack[-1] for stack in boxes]))
 
 if __name__ == "__main__":
     with open('input/day05.txt') as f:
