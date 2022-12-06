a755fa520c90d45f92b71d7cf7ae49502f7edabd
Jakob Madsen <>
2022-12-06 09:48:05 +0100

    Update day06.py

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,4 +1,4 @@
-def solve(content, length, buffer = ''):
+def solve(content, length):
     print([idx + length for idx, string in enumerate(
           [*zip([zip(content[n:n+length]) for n in range(len(content)-length+1)])])
           if len(set([''.join(''.join(map(lambda x: x[0],a))) for a in string][0])) ==  length
