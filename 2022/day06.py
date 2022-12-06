357a0fb5b1504dc435a48d351845e65331952cb5
Jakob Madsen <>
2022-12-06 10:32:45 +0100

    Update day06.py

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,8 +1,8 @@
 def solve(content, length):
-    print([idx + length for idx, string in enumerate(
-          [*zip([zip(content[n:n+length]) for n in range(len(content)-length+1)])])
-          if len(set([''.join(''.join(map(lambda x: x[0],a))) for a in string][0])) ==  length
-        ][0])
+    print([idx + length for idx, string in 
+        enumerate([*zip([zip(content[n:n+length]) for n in range(len(content)-length+1)])])
+        if len(set([''.join(''.join(map(lambda x: x[0],a))) for a in string][0])) == length
+    ][0])
 
 if __name__ == "__main__":
     with open('input/day06.txt') as f:
