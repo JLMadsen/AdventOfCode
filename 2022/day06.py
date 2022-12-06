edf4b3a2e157328383e6c5cbff151df8a195392f
Jakob Madsen <>
2022-12-06 09:47:41 +0100

    Update day06.py

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,15 +1,11 @@
-def solve(content, length, value = 0, buffer = ''):
-    for idx, char in enumerate(content[0]):
-        buffer += char
-        if len(buffer) == (length + 1): buffer = buffer[1:]
-        if len(set(buffer)) == length:
-            value = idx+1
-            break
-
-    print(value)
+def solve(content, length, buffer = ''):
+    print([idx + length for idx, string in enumerate(
+          [*zip([zip(content[n:n+length]) for n in range(len(content)-length+1)])])
+          if len(set([''.join(''.join(map(lambda x: x[0],a))) for a in string][0])) ==  length
+        ][0])
 
 if __name__ == "__main__":
     with open('input/day06.txt') as f:
-        content = f.read().splitlines()
+        content = f.read().splitlines()[0]
         solve(content, 4) # 1598
         solve(content, 14) # 2414
