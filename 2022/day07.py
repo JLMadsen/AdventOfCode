58fa7c13c67c35b5d0f727f134c70ef66844f059
Jakob <jakob.m@dsen.tv>
2022-12-07 19:12:38 +0100

    Update day07.py

--- 2022/day07.py
+++ 2022/day07.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-def solve(content, directories = defaultdict(lambda: 0), path = ['/']):
+def solve(content, dirs = defaultdict(lambda: 0), path = ['/']):
     for command in content:
         if '$' in (args := command.split(' ')):
             if args[1] == 'cd':
@@ -10,13 +10,10 @@ def solve(content, directories = defaultdict(lambda: 0), path = ['/']):
         elif args[0] == 'dir': continue
         else:
             for i in range(len(path)):
-                directories[str(path[:i+1])] += int(args[0])
+                dirs[str(path[:i+1])[2:-2]] += int(args[0])
 
-    print(sum([value for value in directories.values() # 1611443
-        if value <= 100_000]))
-        
-    print(min([value for value in directories.values() # 2086088
-        if value >= directories["['/']"] - (70_000_000 - 30_000_000)]))
+    print(sum([v for v in dirs.values() if v <= 1e5])) # 1611443
+    print(min([v for v in dirs.values() if v >= dirs['/'] - 4e7])) # 2086088
 
 if __name__ == "__main__":
     with open('input/day07.txt') as f:
