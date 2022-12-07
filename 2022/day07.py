7a2a10ae88b6f63da68bf7446318fa6d0d51d532
Jakob Madsen <>
2022-12-07 15:30:30 +0100

    Update day07.py

--- 2022/day07.py
+++ 2022/day07.py
@@ -1,42 +1,22 @@
 from collections import defaultdict
 
-def solve(content):
-    directories = defaultdict(lambda: 0)
-    path = ['/']
-
+def solve2(content, directories = defaultdict(lambda: 0), path = ['/']):
     for command in content:
-        args = command.split(' ')
-
-        if '$' in args:
+        if '$' in (args := command.split(' ')):
             if args[1] == 'cd':
-                target = args[2]
-                if target == '..':  path.pop()
-                elif target == '/': path = ['/']
-                else:               path.append(target)
-
+                if (target := args[2]) == '..': path.pop()
+                elif target == '/':             path = ['/']
+                else:                           path.append(target)
+        elif args[0] == 'dir': continue
         else:
-            if args[0] == 'dir': continue
-            size, filename = args
-            directories[''.join(path)] += int(size)
+            for i in range(len(path)):
+                directories[str(path[:i+1])] += int(args[0])
 
-            for idx in range(1, len(path)):
-                tmp_path = ''.join(path[:-idx])
-                directories[tmp_path] += int(size)
-
-    value = 0
-    for d, v in directories.items():
-        if v <= 100_000:
-            value += v
-
-    print(value) # 1611443
-
-    min_dir_size = float('inf')
-    total = directories['/'] - 40_000_000
-    for d, v in directories.items():
-        if v >= total and v < min_dir_size:
-            min_dir_size = v
-            
-    print(min_dir_size) # 2086088
+    print(sum([value for value in directories.values() # 1611443
+        if value <= 100_000]))
+        
+    print(min([value for value in directories.values() # 2086088
+        if value >= directories["['/']"] - (70_000_000 - 30_000_000)]))
 
 if __name__ == "__main__":
     with open('input/day07.txt') as f:
