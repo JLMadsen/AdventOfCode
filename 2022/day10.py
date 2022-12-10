d0b87312d85c7acde700acfc9e57b79f5e03be3f
Jakob <42341254+JLMadsen@users.noreply.github.com>
2022-12-10 08:56:17 +0100

    Create day10.py

---
+++ 2022/day10.py
@@ -0,0 +1,33 @@
+p = [20, 60, 100, 140, 180, 220]
+
+def solve(content, value = 0, register = 1, 
+          cycle = 0, monitor = ""):
+
+    for line in content:
+        mod = lambda x: (x+1)//40
+        pxl = lambda: abs(cycle -1- (register + 40 * mod(cycle))) < 2
+
+        def cycle_once():
+            nonlocal cycle, value, monitor
+            cycle += 1
+            if cycle in p: value += cycle * register
+            monitor += '#' if pxl() else '.'
+
+        if line == "noop":
+            cycle_once()
+
+        else:
+            cycle_once()
+            cycle_once()
+            
+            register += int(line.split()[1])
+
+    print(value) # 14720
+    print('', *[''.join([ c + ('.' if (i+1)%5==0 else '') 
+        for i,c in enumerate(monitor[i*40:i*40+40])]) + '\n' 
+            for i in range(6)]) # FZBPBFZF
+
+if __name__ == '__main__':
+    with open('./input.txt') as f:
+        content = f.read().splitlines()
+        solve(content)
