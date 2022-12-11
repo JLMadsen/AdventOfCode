d61fe4f2601bf2b9a3a47f24327b8fad9c03255b
Jakob <jakob.m@dsen.tv>
2022-12-11 21:39:35 +0100

    Update day11.py

--- 2022/day11.py
+++ 2022/day11.py
@@ -2,16 +2,18 @@ from math import prod
 
 def solve(content, pt2 = False, n=7, mod=1, monkeys={}):
     for idx in range(( len(content)//n) +1 ):
-        monkey = content[idx*n:idx*n+n]
-        mod *= int(monkey[3].split()[-1])
-        monkeys[str(idx)] = {'items': [*map(lambda m: int(m.replace(',','')), monkey[1].split()[2:])],
-                        'operation': monkey[2].split()[4:], 
-                        'test': int(monkey[3].split()[-1]), 
-                        'throw_True': monkey[4].split()[-1], 
-                        'throw_False': monkey[5].split()[-1], 
-                        'count': 0}
+        monkey = [*map(lambda s: s.split() ,content[idx*n:idx*n+n])]
+        mod *= int(monkey[3][-1])
+        monkeys[str(idx)] = {
+            'items': [*map(lambda m: int(m.replace(',','')), monkey[1][2:])],
+            'operation': monkey[2][4:], 
+            'test': int(monkey[3][-1]), 
+            'throw_True': monkey[4][-1], 
+            'throw_False': monkey[5][-1], 
+            'count': 0
+        }
 
-    for round in range(20 if not pt2 else 10_000):
+    for _ in range(20 if not pt2 else 10_000):
         for idx in range(len(monkeys)):
             m = monkeys[str(idx)]
             op, v = m['operation']
@@ -23,7 +25,7 @@ def solve(content, pt2 = False, n=7, mod=1, monkeys={}):
                 monkeys[m['throw_' + str(new_worry%m['test']==0)]]['items'].append(new_worry)
                 monkeys[str(idx)]['items'].remove(item)
 
-    print(prod(sorted([v['count'] for v in monkeys.values()], reverse=True)[:2]))
+    print(prod(sorted([v['count'] for v in monkeys.values()])[::-1][:2]))
 
 if __name__ == "__main__":
     with open('input/day11.txt') as f:
