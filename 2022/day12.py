1de88d06ac4b888688463b5e81d4a8d33a7901dc
Jakob <jakob.m@dsen.tv>
2022-12-12 07:11:33 +0100

    day 12

---
+++ 2022/day12.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+from queue import PriorityQueue
+
+START = 'S'
+GOAL = 'E'
+
+def adjacent(size, i, j):
+    return [[y, x] for a, b in [(1,0), (0,1), (-1,0),(0,-1)]
+     if not ((x := a + j) < 0  or 
+             (y := b + i) < 0  or 
+              y >= size[0] + 1 or
+              x >= size[1] + 1 )]
+
+def dijkstra(grid, pt2 = False):
+    size = (len(grid) - 1, len(grid[0]) - 1)
+    start = (0, 0)
+    pt2_start = []
+    goal = (0, 0)
+
+    for y, row in enumerate(grid):
+        for x, v in enumerate(row):
+            if v == START:
+                start = (y, x)
+            if v == GOAL:
+                goal = (y, x)
+            if v == 'a':
+                pt2_start.append((y,x))
+
+    Q = PriorityQueue()
+
+    if not pt2:
+        Q.put((0, start))
+    else:
+        for point in pt2_start:
+            Q.put((0, point))
+
+    dist = defaultdict(lambda: float('inf'))
+    if not pt2:
+        dist[start] = 0
+    else:
+        for point in pt2_start:
+            dist[point] = 0
+
+    while not Q.empty():
+        _, (y1, x1) = Q.get()
+        height = grid[y1][x1]
+
+        if (y1, x1) == goal:
+            print(dist[goal])
+            return
+
+        for y2, x2 in adjacent(size, y1, x1):
+            if y2 > size[0] or x2 > size[1]:
+                continue  
+
+            new_distance = dist[(y1, x1)] + 1
+            next_height = grid[y2][x2]
+
+            delta_height = ord(next_height) - ord(height) if height != START else -1
+            if next_height == GOAL and height != 'z':
+                continue
+            if new_distance < dist[(y2, x2)] and delta_height < 2:
+                dist[(y2, x2)] = new_distance
+                Q.put((new_distance, (y2, x2)))
+
+if __name__ == "__main__":
+    with open('input/day12.txt') as f:
+        content = f.read().splitlines()
+        dijkstra(content) # 484
+        dijkstra(content, True) # 478
