a4c23939a1974e6925f0e53fcb9a1014ab702c5b
Jakob Madsen <>
2022-12-14 11:03:52 +0100

    part 1

--- 2022/day14.py
+++ 2022/day14.py
@@ -1,22 +1,15 @@
 nth = lambda arr, n: [*zip(*arr)][n-1]
+xmin, xmax, ymin, ymax = 0, 0, 0, 0
 
-def print_waterfall(walls):
-    xmin, xmax = min(nth([*walls], 1)), max(nth([*walls], 1)) + 1
-    ymin, ymax = min(nth([*walls], 0)), max(nth([*walls], 0))
-
-    ymin = 0
-    ymax += 1
-
-    print(xmin, xmax)
-    print(ymin, ymax)
+def print_waterfall(walls, sands):
 
     print('_'*(ymax-ymin+5))
-    print(' ', ''.join([str(i)[-1] for i in range(xmin, xmax)]))
+    print(' ', ' '.join([str(i)[-1] for i in range(xmin, xmax)]))
 
     for dy in range(ymin, ymax):
         print(f'{dy} ', end='')
         for dx in range(xmin, xmax):
-            print('#' if (dx, dy) in walls else '.', end='')
+            print('⬜' if (dx, dy) in walls else ('⬛' if (dx, dy) in sands else '  '), end='')
         print()
 
     print('_'*(ymax-ymin+5))
@@ -53,13 +46,20 @@ def part1(content):
                     # print('>',(dx, ay))
                     walls.add((dx, ay))
 
-    print(walls)
-    print_waterfall(walls)
+    global xmin, xmax, ymin, ymax
+    xmin, xmax = min(nth([*walls], 1)), max(nth([*walls], 1)) + 1
+    ymin, ymax = min(nth([*walls], 0)), max(nth([*walls], 0)) + 1
+    ymin = 0
 
-    while 1:
+    print(walls)
+    # print_waterfall(walls, sands)
+
+    done = False
+    while not done:
 
         sand = (500, 0)
         rest = False
+
 
         while not rest:
             down        = (sand[0],     sand[1] + 1)
@@ -67,14 +67,22 @@ def part1(content):
             down_right  = (sand[0] + 1, sand[1] + 1)
 
             for next_pos in [down, down_left, down_right]:
-                if next_pos not in walls or next_pos not in sands:
-                    pass
+                if next_pos not in walls and next_pos not in sands:
+                    sand = next_pos
+                    break
+            else:
+                sands.add(sand)
+                rest = True
+                break
+
+            if sand[1] > ymax:
+                done = True
+                break
+
+        # print_waterfall(walls, sands)
 
 
-            break
-        break
-
-    print(value)
+    print(len(sands))
 
 def part2(content):
     pass
@@ -85,7 +93,7 @@ ex = """498,4 -> 498,6 -> 496,6
 if __name__ == "__main__":
     with open('input/day14.txt') as f:
         content = f.read().splitlines()
-        content = ex.splitlines()
+        # content = ex.splitlines()
 
         part1(content)
         part2(content)
