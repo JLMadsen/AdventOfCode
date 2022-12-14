4de54ab575187dc391ff51da9f10264195e66c64
Jakob Madsen <>
2022-12-14 12:38:26 +0100

    finish day 14

--- 2022/day14.py
+++ 2022/day14.py
@@ -7,15 +7,17 @@ def print_waterfall(walls, sands):
     print(' ', ' '.join([str(i)[-1] for i in range(xmin, xmax)]))
 
     for dy in range(ymin, ymax):
-        print(f'{dy} ', end='')
+        print(f'{dy}'.rjust(4), end='')
         for dx in range(xmin, xmax):
-            print('⬜' if (dx, dy) in walls else ('⬛' if (dx, dy) in sands else '  '), end='')
+            print('⬜' if (dx, dy) in walls else (
+                  '💛' if (dx, dy) in sands else 
+                  '⬛'), end='')
         print()
 
     print('_'*(ymax-ymin+5))
 
 
-def part1(content):
+def solve(content, pt2 = False):
 
     walls = set()
     sands = set()
@@ -27,23 +29,17 @@ def part1(content):
                 [*map(int, n.split(','))], 
                 line.split(' -> '))]
 
-        print(steps)
-
         for i in range(len(steps) - 1):
             (ax, ay), (bx, by) = steps[i], steps[i+1]
-            # print(ax, ay, '->', bx, by)
-            # print('diff', abs(ax-bx), abs(ay-by))
 
             if ax == bx:
                 for n in range(abs(ay - by) + 1):
                     dy = ay + (n if ay < by else -n)
-                    # print('>',(ax, dy))
                     walls.add((ax, dy))
 
             if ay == by:
                 for n in range(abs(ax - bx) + 1):
                     dx = ax + (n if ax < bx else -n)
-                    # print('>',(dx, ay))
                     walls.add((dx, ay))
 
     global xmin, xmax, ymin, ymax
@@ -51,15 +47,11 @@ def part1(content):
     ymin, ymax = min(nth([*walls], 0)), max(nth([*walls], 0)) + 1
     ymin = 0
 
-    print(walls)
-    # print_waterfall(walls, sands)
-
     done = False
     while not done:
 
         sand = (500, 0)
         rest = False
-
 
         while not rest:
             down        = (sand[0],     sand[1] + 1)
@@ -67,33 +59,25 @@ def part1(content):
             down_right  = (sand[0] + 1, sand[1] + 1)
 
             for next_pos in [down, down_left, down_right]:
-                if next_pos not in walls and next_pos not in sands:
+
+                if next_pos not in walls and next_pos not in sands and (next_pos[1] < ymax + 1 if pt2 else 1):
                     sand = next_pos
                     break
             else:
+                if pt2 and sand == (500, 0):
+                    done = True
                 sands.add(sand)
                 rest = True
                 break
 
-            if sand[1] > ymax:
+            if sand[1] > ymax and not pt2:
                 done = True
                 break
 
-        # print_waterfall(walls, sands)
-
-
     print(len(sands))
-
-def part2(content):
-    pass
-
-ex = """498,4 -> 498,6 -> 496,6
-503,4 -> 502,4 -> 502,9 -> 494,9"""
 
 if __name__ == "__main__":
     with open('input/day14.txt') as f:
         content = f.read().splitlines()
-        # content = ex.splitlines()
-
-        part1(content)
-        part2(content)
+        solve(content) # 578
+        solve(content, True) # 24377
