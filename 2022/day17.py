80daf681b7a6318f3a0c538a88418adb3c4bbe61
Jakob <jakob.m@dsen.tv>
2022-12-18 20:00:44 +0100

    Update day17.py

--- 2022/day17.py
+++ 2022/day17.py
@@ -1,9 +1,3 @@
-
-import math
-import re
-from collections import defaultdict
-nth = lambda arr, n: [*zip(*arr)][n-1]
-
 SHAPES = [
     ["####"], 
 
@@ -32,32 +26,26 @@ def print_tetris(grid):
         print('|')
     print('+'+('--'*7)+'+')
 
-def part1(content):
+def solve(content, n_rocks):
     
     rock_id = 0
     
     grid = set()
     global width; width = 7
     highest_point = 0
-    pieces = 2021
     direction_idx = 0
 
-    # for direction in content:
-    for n in range(pieces + 1):
+    from tqdm import trange
+
+    for n in trange(n_rocks):
         rock = set()
         points = SHAPES[n % len(SHAPES)]
-        # print('Rock', n)
         for y, row in enumerate(points[::-1]):
             for x, char in enumerate(row):
                 if char == '#':
                     rock.add((x + 2, y + highest_point + 3))
 
-        # print_tetris(grid | rock)
-
         while True:
-            # print_tetris(grid | rock)
-            # print()
-
             direction = content[direction_idx % len(content)]
             direction_idx += 1
 
@@ -82,27 +70,20 @@ def part1(content):
 
                 rock = set([(x, y-1) for (x, y) in rock])
             else:
-                # print(rock, direction)
                 grid |= rock
 
                 for (x, y) in rock:
                     highest_point = max(highest_point, y + 1)
 
-                # print_tetris(grid)
-                # exit()
                 break
 
-            
     print(highest_point)
 
 def part2(content):
     pass
 
-ex = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
-
 if __name__ == "__main__":
     with open('input/day17.txt') as f:
         content = f.read().splitlines()[0]
-        # content = ex
-        part1(content)
-        part2(content)
+        solve(content, 2022) # 3055
+        solve(content, 1000000000000)
