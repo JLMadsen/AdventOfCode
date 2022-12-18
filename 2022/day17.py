0cd0ab7ac9909de94813830b158b257dea209437
Jakob <jakob.m@dsen.tv>
2022-12-18 19:52:01 +0100

    day 17 part 1

--- 2022/day17.py
+++ 2022/day17.py
@@ -27,8 +27,8 @@ SHAPES = [
 def print_tetris(grid):
     for y in range(10, -1, -1):
         print('|', end='')
-        for x in range(width):
-            print('⬜' if (x+1,y) in grid else '◾', end='')
+        for x in range(width ):
+            print('⬜' if (x,y) in grid else '◾', end='')
         print('|')
     print('+'+('--'*7)+'+')
 
@@ -39,22 +39,24 @@ def part1(content):
     grid = set()
     global width; width = 7
     highest_point = 0
-    pieces = 2022
+    pieces = 2021
     direction_idx = 0
 
     # for direction in content:
-    for n in range(pieces):
+    for n in range(pieces + 1):
         rock = set()
         points = SHAPES[n % len(SHAPES)]
-        print('Rock', n)
+        # print('Rock', n)
         for y, row in enumerate(points[::-1]):
             for x, char in enumerate(row):
                 if char == '#':
-                    rock.add((x + 3, y + highest_point + 3))
+                    rock.add((x + 2, y + highest_point + 3))
+
+        # print_tetris(grid | rock)
 
         while True:
-            print_tetris(grid | rock)
-            print()
+            # print_tetris(grid | rock)
+            # print()
 
             direction = content[direction_idx % len(content)]
             direction_idx += 1
@@ -63,11 +65,11 @@ def part1(content):
                 for (x, y) in rock:
                     dx = x + 1
                     if (dx, y) in grid: break
-                    if dx > width: break
+                    if dx >= width:     break
                 else:
                     rock = set([(x+1,y) for (x,y) in rock])
 
-            if direction == '<':
+            elif direction == '<':
                 for (x, y) in rock:
                     dx = x - 1
                     if (dx, y) in grid: break
@@ -80,19 +82,17 @@ def part1(content):
 
                 rock = set([(x, y-1) for (x, y) in rock])
             else:
-                print(rock, direction)
+                # print(rock, direction)
                 grid |= rock
 
                 for (x, y) in rock:
                     highest_point = max(highest_point, y + 1)
 
-                print_tetris(grid)
+                # print_tetris(grid)
                 # exit()
                 break
 
             
-                
-
     print(highest_point)
 
 def part2(content):
@@ -103,6 +103,6 @@ ex = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 if __name__ == "__main__":
     with open('input/day17.txt') as f:
         content = f.read().splitlines()[0]
-        content = ex
+        # content = ex
         part1(content)
         part2(content)
