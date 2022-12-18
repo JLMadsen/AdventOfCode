7fc225624059fa1af4746d69818e4974de01f830
Jakob <jakob.m@dsen.tv>
2022-12-18 22:16:07 +0100

    Update day17.py

--- 2022/day17.py
+++ 2022/day17.py
@@ -26,24 +26,22 @@ def print_tetris(grid):
         print('|')
     print('+'+('--'*7)+'+')
 
-def solve(content, n_rocks):
-    
-    rock_id = 0
-    
-    grid = set()
+def solve(content, n_rocks, grid = set()):
     global width; width = 7
     highest_point = 0
     direction_idx = 0
 
-    from tqdm import trange
-
-    for n in trange(n_rocks):
+    for n in range(n_rocks):
         rock = set()
-        points = SHAPES[n % len(SHAPES)]
-        for y, row in enumerate(points[::-1]):
+        for y, row in enumerate(SHAPES[n % len(SHAPES)][::-1]):
             for x, char in enumerate(row):
                 if char == '#':
                     rock.add((x + 2, y + highest_point + 3))
+
+        if (highest_point > 0 and
+            n % len(SHAPES) == 0 and
+            direction_idx == 0 ):
+            print('LOOP')
 
         while True:
             # print_tetris(grid | rock)
@@ -52,30 +50,24 @@ def solve(content, n_rocks):
 
             if direction == '>':
                 for (x, y) in rock:
-                    dx = x + 1
-                    if (dx, y) in grid: break
-                    if dx >= width:     break
+                    if (x+1, y) in grid or x+1 >= width:
+                        break
                 else:
                     rock = set([(x+1,y) for (x,y) in rock])
 
-            elif direction == '<':
+            if direction == '<':
                 for (x, y) in rock:
-                    dx = x - 1
-                    if (dx, y) in grid: break
-                    if dx < 0:          break
+                    if (x-1, y) in grid or x-1 < 0:
+                        break
                 else:
                     rock = set([(x-1,y) for (x,y) in rock])
 
-            if not any( ((x, y-1) in grid or 
-                        ((y-1) < 0)) for (x,y) in rock ):
-
+            if not any( ((x, y-1) in grid or ((y-1) < 0)) for (x,y) in rock ):
                 rock = set([(x, y-1) for (x, y) in rock])
             else:
                 grid |= rock
-
                 for (x, y) in rock:
                     highest_point = max(highest_point, y + 1)
-
                 break
 
     print_tetris(grid)
@@ -88,4 +80,4 @@ if __name__ == "__main__":
     with open('input/day17.txt') as f:
         content = f.read().splitlines()[0]
         solve(content, 2022) # 3055
-        solve(content, 1000000000000)
+        # solve(content, 1000000000000)
