a36a8b75a87c8943732dc53e822c9ff7ac68e282
Jakob <jakob.m@dsen.tv>
2022-12-20 10:23:11 +0100

    Update day20.py

--- 2022/day20.py
+++ 2022/day20.py
@@ -1,29 +1,23 @@
 from collections import deque
-# https://docs.python.org/3/library/collections.html#collections.deque
 
-def solve(content, pt2 = False):
+def solve(content, pt2 = False, current_value = 0):
     content = deque([*map(lambda n: int(n) * (811589153 if pt2 else 1), content)])
     indexes = deque(range(0, (length := len(content))))
 
     for _ in range(10 if pt2 else 1):
         for idx in range(length):
             position = indexes.index(idx)
-            
-            content.rotate(position * -1)
-            current_value = content.popleft()
-            content.rotate(current_value * -1)
-            content.appendleft(current_value)
-
-            indexes.rotate(position * -1)
-            current_index = indexes.popleft()
-            indexes.rotate(current_value * -1)
-            indexes.appendleft(current_index)
+            for deq in [content, indexes]:
+                deq.rotate(position * -1)
+                local_value = deq.popleft()
+                if deq == content: current_value = local_value
+                deq.rotate(current_value * -1)
+                deq.appendleft(local_value)
 
     zero = content.index(0)
     a, b, c = (content[(zero + 1000) % (len(content))],
                content[(zero + 2000) % (len(content))],
                content[(zero + 3000) % (len(content))])
-
     print(sum([a,b,c]))
 
 if __name__ == "__main__":
