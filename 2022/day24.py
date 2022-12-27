85fbd23d93fb03a3b0e485935a83f028463aa564
Jakob <jakob.m@dsen.tv>
2022-12-27 21:23:07 +0100

    day 24, example working

--- 2022/day24.py
+++ 2022/day24.py
@@ -1,51 +1,48 @@
 from collections import defaultdict
 
+def adjacent(x, y):
+    return [(x    , y    ), (x    , y    ),
+            (x + 1, y    ), (x - 1, y    ),
+            (x    , y + 1), (x    , y - 1)]
+
 directions = {
     '^': (0, -1),
     'v': (0,  1),
     '<': (-1, 0),
-    '>': ( 1, 0)
-}
+    '>': ( 1, 0)}
 
-dir_to_emoji = lambda d: '🔼🔽⏩⏪'['^v><'.index(d)]
-
-nth = lambda arr, n: [*zip(*arr)][n-1]
 def print_blizzard():
-    for y in range(ymax):
-        for x in range(xmax):
+    for y in range(ymax + 1):
+        for x in range(xmax + 1):
             point = (x, y)
-            char = '⬛'
-            if point in walls:
-                char = '⬜'
-            if point == player:
-                char = '👽'
-            if point == goal:
-                char = '🥅'
+            char = '.'
+            if point in walls: char ='#'
+            if point in elves: char = 'P'
+            if point == goal:  char = 'E'
             if point in tornado_set:
                 dirs = tornado_dir[point]
-                char = dir_to_emoji(dirs[0]) if len(dirs) == 1 else str(len(dirs)).rjust(2)
+                char = dirs[0] if len(dirs) < 2 else len(dirs)
             print(char, end='')
         print()
+    print()
 
 def solve(content, steps = 0):
-    global walls, player, goal, tornado_set, tornado_dir, xmax, ymax, players
+    global walls, start, goal, tornado_set, tornado_dir, xmax, ymax, elves
     walls = set()
-    players = set()
-    player = (0, 0)
-    goal = (0, 0)
+    goal = start = (0, 0)
 
     tornado_set = set()
     tornado_dir = defaultdict(lambda: []) # (x, y): ['<','V']
 
     ymax = xmax = 0
     for y, line in enumerate(content):
-        ymax = max(ymax, y + 1)
+        ymax = max(ymax, y)
         for x, value in enumerate(line):
-            xmax = max(xmax, x + 1)
+            xmax = max(xmax, x)
             point = (x, y)
 
             if y == 0 and value == '.':
-                player = point
+                start = point
 
             if y == (len(content) -1) and value == '.':
                 goal = point
@@ -57,18 +54,10 @@ def solve(content, steps = 0):
                 tornado_set.add(point)
                 tornado_dir[point].append(value)
 
-    print(xmax, ymax)
-
-    # print(player, goal)
-    # print_blizzard()
-
-    # state
-    #   minute
-    #   position
+    elves = set([start])
 
     def step_blizzard():
         global tornado_dir, tornado_set
-
         new_tornado_set = set()
         new_tornado_dir = defaultdict(lambda: [])
 
@@ -80,10 +69,10 @@ def solve(content, steps = 0):
                 dx = tornado[0] + delta[0]
                 dy = tornado[1] + delta[1]
 
-                if   dx == 0:        dx = xmax - 2
-                elif dx >= xmax - 1: dx = 1
-                elif dy == 0:        dy = ymax - 2
-                elif dy >= ymax - 1: dy = 1
+                if   dx == 0:    dx = xmax - 1
+                elif dx >= xmax: dx = 1
+                elif dy == 0:    dy = ymax - 1
+                elif dy >= ymax: dy = 1
 
                 delta_tornado = (dx, dy)
                 
@@ -93,26 +82,31 @@ def solve(content, steps = 0):
         tornado_set = new_tornado_set
         tornado_dir = new_tornado_dir
 
-    print_blizzard()
+    def step_elves():
+        global elves
+        new_elves = set([start])
 
-    # state = [(player, 0)]
+        for elf in elves:
+            for delta_elf in adjacent(*elf):
+                if delta_elf == goal: return True
+                if (delta_elf not in walls and 
+                    delta_elf not in tornado_set):
+                    new_elves.add(delta_elf)
+        
+        elves = new_elves
+        return False
 
     reached_goal = False
+    minute = 0
+
     while not reached_goal:
+        # print_blizzard()
         step_blizzard()
-        # step_blizzard()
-        # step_blizzard()
-        print_blizzard()
+        reached_goal = step_elves()
+        minute += 1
 
-        exit()
-
-
-
-
-    
-
-                
-
+    print(minute)
+    # 153 < 
 
 ex = """#.######
 #>>.<^<#
@@ -120,7 +114,6 @@ ex = """#.######
 #>v.><>#
 #<^v^^>#
 ######.#"""
-
 
 if __name__ == "__main__":
     with open('input/day24.txt') as f:
