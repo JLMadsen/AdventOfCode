ff6c0a9aac4ba9f18d666d216a29f4af5a8b8115
Jakob <jakob.m@dsen.tv>
2023-12-03 11:23:00 +0100

    Update day03.py

--- 2023/day03.py
+++ 2023/day03.py
@@ -16,25 +16,22 @@ def solve(content, total=0, ratios=0):
                     for dj in [-1,0,1]:
                         try:
                             adj = content[i+di][dj+j]
-                            if not adj.isdigit() and adj != '.':
-                                has_symbol = True
-                            if adj == '*':
-                                gear_pos=(i+di,dj+j)
+                            has_symbol += (not adj.isdigit() and adj != '.')
+                            if adj == '*': gear_pos=(i+di,dj+j)
                         except: pass
 
             else:
-                if has_symbol: total += int(buffer)
-                if gear_pos:   gears[gear_pos].append(int(buffer))
-                buffer = ''
-                has_symbol = False
-                gear_pos = None
+                if has_symbol: 
+                    total += int(buffer)
+                if gear_pos:   
+                    gears[gear_pos].append(int(buffer))
+                    gear_pos = None
+                buffer, has_symbol = '', 0             
     
-    for n in gears.values():
-        if len(n) > 1:
-            ratios += math.prod(n)
+    ratios = sum([math.prod(v)*(len(v)>1) for v in gears.values()])
 
-    print(total)
-    print(ratios)
+    print(total)  # 507214
+    print(ratios) # 72553319
 
 if __name__ == "__main__":
     with open('input/day03.txt') as f:
