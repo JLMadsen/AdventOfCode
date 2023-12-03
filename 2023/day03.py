bb9dcb5697e7ce8d4e1d4f11b39cb34d86d80c16
Jakob <jakob.m@dsen.tv>
2023-12-03 11:15:29 +0100

    Update day03.py

--- 2023/day03.py
+++ 2023/day03.py
@@ -1,14 +1,15 @@
 import math
 from collections import defaultdict
 
-def part1(content, value = 0):
-    
+def solve(content, total=0, ratios=0):
+    buffer      = ''
+    has_symbol  = False
+    gear_pos    = None
+    gears       = defaultdict(lambda: [])
 
-    nums = []
-    buffer = ''
-    has_symbol = False
     for i, line in enumerate(content):
         for j, char in enumerate(line):
+
             if char.isdigit():
                 buffer += char
                 for di in [-1,0,1]:
@@ -17,49 +18,25 @@ def part1(content, value = 0):
                             adj = content[i+di][dj+j]
                             if not adj.isdigit() and adj != '.':
                                 has_symbol = True
-                        except: pass
-            elif not char.isdigit():
-                if has_symbol:
-                    nums.append(int(buffer))
-                buffer = ''
-                has_symbol = False
-    print(sum(nums))
-
-def part2(content):
-    value = 0
-
-    nums = []
-    buffer = ''
-    has_symbol = False
-    gear_pos = None
-    gears = defaultdict(lambda: [])
-    for i, line in enumerate(content):
-        for j, char in enumerate(line):
-            if char.isdigit():
-                buffer += char
-                for di in [-1,0,1]:
-                    for dj in [-1,0,1]:
-                        try:
-                            adj = content[i+di][dj+j]
                             if adj == '*':
-                                has_symbol = True
                                 gear_pos=(i+di,dj+j)
                         except: pass
-            elif not char.isdigit():
-                if has_symbol:
-                    gears[gear_pos].append(int(buffer))
-                    nums.append(int(buffer))
+
+            else:
+                if has_symbol: total += int(buffer)
+                if gear_pos:   gears[gear_pos].append(int(buffer))
                 buffer = ''
                 has_symbol = False
+                gear_pos = None
     
     for n in gears.values():
         if len(n) > 1:
-            value += math.prod(n)
+            ratios += math.prod(n)
 
-    print(value)
+    print(total)
+    print(ratios)
 
 if __name__ == "__main__":
     with open('input/day03.txt') as f:
         content = f.read().splitlines()
-        part1(content) # 507214
-        part2(content) # 72553319
+        solve(content)
