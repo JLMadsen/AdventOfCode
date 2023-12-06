2f77185bf7f447b556cef7c613bc157740fb5c49
Jakob <jakob.m@dsen.tv>
2023-12-06 10:14:09 +0100

    Update day06.py

--- 2023/day06.py
+++ 2023/day06.py
@@ -1,8 +1,7 @@
-def solve(content, pt2=0, value=1):
-    content = [*map(lambda y: [int(''.join(y))] if pt2 else [*map(int, y)],map(lambda x: x.split(':')[-1].split(), content))]
-    for t, d in zip(*content):
-        value *= sum([i*(t-i) > d for i in range(t)])
-    print(value)
+import math
+
+def solve(content, pt2=0):
+    print(math.prod([sum([i*(t-i)>d for i in range(t)]) for t,d in zip(*map(lambda y:[int(''.join(y))] if pt2 else[*map(int, y)],map(lambda x:x.split(':')[-1].split(),content)))]))
 
 if __name__ == "__main__":
     with open('input/day06.txt') as f:
