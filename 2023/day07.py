ef8e4354f17901924e62b3cb838301caaf4a9022
Jakob <jakob.m@dsen.tv>
2023-12-07 10:55:34 +0100

    Update day07.py

--- 2023/day07.py
+++ 2023/day07.py
@@ -1,27 +1,25 @@
 from functools import cmp_to_key
 
-types = {'five': lambda x: len(set(x))==1,
-         'four': lambda x: x.count(x[0]) == 4 or x.count(x[1]) == 4,
-         'full': lambda x: len(set(x))==2 and x.count(x[0]) in [2,3],
-         'three': lambda a: a.count(max(a, key=lambda x: a.count(x))) == 3,
-         'two': lambda x: len(set(x))==3,
-         'one': lambda x: len(set(x))==4,
-         'high': lambda x: len(set(x))==5}
+types = {'five':  lambda x: len(set(x))==1,
+         'four':  lambda x: x.count(x[0])==4 or x.count(x[1])==4,
+         'full':  lambda x: len(set(x))==2 and x.count(x[0]) in [2,3],
+         'three': lambda a: a.count(max(a,key=lambda x: a.count(x)))==3,
+         'two':   lambda x: len(set(x))==3,
+         'one':   lambda x: len(set(x))==4,
+         'high':  lambda x: len(set(x))==5}
 
-def solve(content, pt2=0, value=0, hands={}):
-    cards = '23456789TJQKA' if not pt2 else 'J23456789TQKA'
+def solve(content, pt2=0, hands={}):
+    cards = 'J'*pt2+'23456789TJQKA'.replace('J'*pt2,'')
 
     def parse_hand(hand):
-        for key, func in types.items():
+        for i, (key, func) in enumerate(types.items()):
             if pt2:
                 for card in cards[::-1]:
-                    new_hand = hand.replace('J', card)
-                    if func(new_hand):
-                        return [*types.keys()].index(key)
+                    if func(hand.replace('J', card)):
+                        return i
             else:
                 if func(hand):
-                    return [*types.keys()].index(key)
-        return -1
+                    return i
 
     for line in content:
         hand, bid = line.split()
@@ -37,8 +35,7 @@ def solve(content, pt2=0, value=0, hands={}):
                     return 1
                 elif cards.index(da) > cards.index(db):
                     return -1
-        else:
-            return a_type - b_type
+        else:       return a_type - b_type
 
     order = sorted([*hands.keys()], key=cmp_to_key(compare))
     print(sum([(i+1) * hands[h][0] for i,h in enumerate(order[::-1])]))
