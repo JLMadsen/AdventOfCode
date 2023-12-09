621d335fb415ba39c88d555d915c7c2b0a351187
Jakob <jakob.m@dsen.tv>
2023-12-09 12:00:43 +0100

    Update day09.py

--- 2023/day09.py
+++ 2023/day09.py
@@ -3,14 +3,11 @@ def solve(content, pt2=0, value=0):
         diff = [[*map(int, line)]]
 
         while not all(n == 0 for n in diff[-1]):
-            new_diffs = []
-            for a, b in zip(diff[-1], diff[-1][1:]):
-                new_diffs.append(int(b)-int(a))
-            diff.append(new_diffs)
+            diff.append([b-a for a,b in zip(diff[-1], diff[-1][1:])])
 
         for i in range(len(diff)-1, 0, -1):
             if pt2: diff[i-1].insert(0, diff[i-1][0] - diff[i][0])
-            else:   diff[i-1].append(diff[i][-1]+diff[i-1][-1])
+            else:   diff[i-1].append(   diff[i][-1]  + diff[i-1][-1])
         value += diff[0][0 if pt2 else -1]
 
     print(value)
