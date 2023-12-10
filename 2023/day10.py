49205911efe729943146ca4a3728008ca0184ae8
Jakob <jakob.m@dsen.tv>
2023-12-10 17:34:47 +0100

    Update day10.py

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,35 +1,28 @@
-N,E,S,W = (-1,0),(0,1),(1,0),(0,-1)
-pipes = {'|': [N, S], '-': [E, W],
-         'L': [N, E], 'J': [N, W],
-         '7': [S, W], 'F': [S, E]}
+N,E,S,W = (-1,0), (0,1), (1,0), (0,-1)
+pipes = {'|': [N, S], '-': [E, W], 'L': [N, E], 
+         'J': [N, W], '7': [S, W], 'F': [S, E]}
 
 def solve(grid, area=0):
-    start = (0, 0)
-    for y,line in enumerate(grid):
-        for x,char in enumerate(line):
-            if char == 'S': start = (y, x)
-
+    start = [(y,l.index('S'))for y,l in enumerate(grid)if'S'in l][0]   
     path = [start]
 
     while 1:
         y, x = path[-1]
 
-        if (y,x) == start:
+        if (y, x) == start:
             if len(path) > 1: break
             for dy, dx in [N,E,S,W]:
                 if grid[y+dy][x+dx] in pipes:
                     (ay, ax),(by, bx) = pipes[grid[y+dy][x+dx]]
-                    if ((y+dy+ay, x+dx+ax) == start or 
-                        (y+dy+by, x+dx+bx) == start): 
+                    if start in [(y+dy+ay,x+dx+ax),(y+dy+by,x+dx+bx)]: 
                         path.append((y+dy, x+dx))
                         area += x * dy
                         break
         else:
             (ay, ax), (by, bx) = pipes[grid[y][x]]
             a, b = (y+ay, x+ax), (y+by, x+bx)
-            target = a if a != path[-2] else b
-            area += x * (ay if target == a else by)
-            path.append(target)
+            path.append(a if a != path[-2] else b)
+            area += x * (ay if path[-1] == a else by)
 
     print(len(path)//2)            # 6979
     print(area - len(path)//2 + 1) # 443
