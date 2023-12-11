d58725bdaa556dd4f1411899b73149842b4d5c8e
Jakob <jakob.m@dsen.tv>
2023-12-11 12:40:35 +0100

    Update day11.py

--- 2023/day11.py
+++ 2023/day11.py
@@ -7,25 +7,25 @@ def solve(content, mul, value=0):
     for line in content:
         row = ""
         for i in range(len(line)):
-            if '#' not in nth(content, i): 
-                row += '@'
+            row += '@'*('#' not in nth(content, i))
             row += line[i]
 
         if '#' not in line: 
-            cosmos.append('@'*len(row))
+            cosmos.append('@')
         cosmos.append(row)
 
     y_times = 0
     for y, row in enumerate(cosmos):
-        if '.' not in row and '#' not in row: y_times += 1
+        if row == '@': y_times += 1; continue
         x_times = 0
         for x, char in enumerate(row):
             if char == '@': x_times += 1
-            if char == '#': galaxies.add((y+((mul-2)*y_times), x+((mul-2)*x_times)))
+            if char == '#': galaxies.add((y+((mul-2)*y_times), 
+                                          x+((mul-2)*x_times)))
 
     for ay, ax in galaxies:
         for by, bx in galaxies:
-            if (ay, ax) == (by, bx ): continue
+            if (ay, ax) == (by, bx): continue
             value += (abs(by-ay)+abs(bx-ax))/2
     print(int(value))
 
