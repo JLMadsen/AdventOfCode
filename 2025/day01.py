012d5ad61735f8c64ab87d525436e198f7ff7d7a
Jakob <jakob.m@dsen.tv>
2025-12-01 23:31:14 +0100

    Update day01.py

--- 2025/day01.py
+++ 2025/day01.py
@@ -1,20 +1,19 @@
-def part1(content, value = 50, zeros = 0):
+def part1(content, value=50, zeros=0):
 
     for d, n in map(lambda l: (l[0], int(l[1:])), content):
-        value = (value + (n * (1 if d == 'R' else -1))) % 100
+        value = (value + (n * (1 - 2 * (d == 'R')))) % 100
         zeros += not value
 
     print(zeros)
 
-def part2(content, value = 50, zeros = 0):
+def part2(content, value=50, zeros=0):
 
     for d, n in map(lambda l: (l[0], int(l[1:])), content):
         r = int(n / 100)
         O = value == 0
-        zeros += r
-        value += ((n - (100 * r)) * (1 if d == 'R' else -1))
-        zeros += (value <= 0 and not O) or value > 99
-        value = value % 100
+        value += (n - (100 * r)) * (1 - 2 * (d == 'R'))
+        zeros += ((value <= 0 and not O) or value > 99) + r
+        value %= 100
 
     print(zeros)
 
